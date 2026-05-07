c0ef45c075a40b4b6ce84e9199a640e3b9668d3e
Daniel <daniel.m.faruk@gmail.com>
2026-05-07 19:54:09 +0400

    Added Start menu

---
+++ Assets/Scripts/Scenes/SceneChanger.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public string sceneToLoad;
+
+
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+}
